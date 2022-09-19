528b1c3cfcc57ac75120d0810f1f4a4bd94634d8
Paul <>
2022-09-19 15:24:14 +0300

    Hacking Puzzle Win State

--- Assets/CoinPuzzleController.cs
+++ Assets/CoinPuzzleController.cs
@@ -1,17 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CoinPuzzleController : MonoBehaviour
 {
+    public bool IsSolved = false;
     CoinController[] coins;
+    public UnityEvent onSolved;
 
     private void Awake()
     {
         coins = GetComponentsInChildren<CoinController>();
+        foreach (CoinController c in coins)
+            c.TieToPuzzle(this);
     }
-    public void TrySwapCoin(CoinController coin)
+    public void TrySwapCoin(CoinController othercoin)
     {
-
+        foreach(CoinController coin in coins)
+        {
+            if (coin.IsEmpty() != othercoin.IsEmpty() && coin != othercoin)
+            {
+                float MinDist = Mathf.Max(coin.SwapRange, othercoin.SwapRange);
+                    if ((coin.RectT.position - othercoin.RectT.position).sqrMagnitude < MinDist * MinDist){
+                    othercoin.Swap(coin);
+                    return;
+                }
+            }
+        }
+    }
+    public void CheckSolved()
+    {
+        if (!IsSolved)
+        {
+            foreach (CoinController coin in coins)
+            {
+if (coin.IsImportant && coin.CoinNumber != coin.RequiresNumber)
+                {
+                    return;
+                }
+            }
+        }
+        Solve();
+    }
+    void Solve()
+    {
+        if (!IsSolved)
+        {
+            IsSolved = true;
+            onSolved.Invoke();
+            Debug.Log("PUZZLE SOLVED!");
+        }
     }
 }
--- Assets/Scripts/CoinPuzzle/CoinController.cs
+++ Assets/Scripts/CoinPuzzle/CoinController.cs
@@ -6,14 +6,19 @@ using UnityEngine.UI;
 
 public class CoinController : MonoBehaviour
 {
+    public CoinPuzzleController puzzleParent;
+    public RectTransform RectT;
     public Image cImage;
     public TextMeshProUGUI cText;
+    public float SwapRange = 100;
     private void Awake()
     {
         if (cImage==null)
-        cImage = GetComponentInChildren<Image>();
+            cImage = transform.GetChild(0).GetComponent<Image>();
         if (cText == null)
             cText = GetComponentInChildren<TextMeshProUGUI>();
+        if (RectT == null)
+            RectT = GetComponent<RectTransform>();
     }
     public int CoinNumber = 0;
     private void Start()
@@ -22,8 +27,37 @@ public class CoinController : MonoBehaviour
     }
     void UpdateCoinDisplay()
     {
-        cImage.enabled = CoinNumber >= 0;
-        cText.enabled = CoinNumber >= 0;
+        cImage.enabled = IsEmpty();
+        cText.enabled = IsEmpty();
         cText.text = CoinNumber.ToString();
     }
+    public void TieToPuzzle(CoinPuzzleController parent)
+    {
+        puzzleParent = parent;
+    }
+    public bool IsEmpty()
+    {
+        return CoinNumber >= 0;
+    }
+    public void OnClicked()
+    {
+        if (puzzleParent != null)
+            puzzleParent.TrySwapCoin(this);
+    }
+    public void Swap(CoinController otherC)
+    {
+        int coinA = CoinNumber;
+        int coinB = otherC.CoinNumber;
+
+        CoinNumber = coinB;
+        otherC.CoinNumber = coinA;
+
+        UpdateCoinDisplay();
+        otherC.UpdateCoinDisplay();
+
+        if (puzzleParent != null)
+            puzzleParent.CheckSolved();
+    }
+    public bool IsImportant = false;
+    public int RequiresNumber = 0;
 }
